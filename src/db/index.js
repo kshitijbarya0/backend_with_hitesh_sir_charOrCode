fc61a73940b772f39180623a41b1fe2d6c62e7b4
kshitijbarya0 <kshitijbarya123@gmail.com>
2024-11-06 21:32:29 +0530

    added the new URL link

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try {
-        const createConnection = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const createConnection = await mongoose.connect(`${process.env.MONGODB_UR}/${DB_NAME}`);
         console.log(`Connection Sucessfull: ${createConnection}`);
     } catch (error) {
         console.error("Error while connecting DataBase: ", error);
